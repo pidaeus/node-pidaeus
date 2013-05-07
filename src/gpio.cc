c5a9922f7334f901af60becec8bc37943d866019
Jake Luer <jake.luer@gmail.com>
2013-05-06 22:38:00 -0400

    debug

--- src/gpio.cc
+++ src/gpio.cc
@@ -83,7 +83,8 @@ GPIO::Teardown(const Arguments &args) {
   GPIO *self = ObjectWrap::Unwrap<GPIO>(args.Holder());
 
   if (self->active) {
-    for (int i = 0; i < PI_MAX_PINS; i++) {
+    size_t i;
+    for (i = 0; i < PI_MAX_PINS; i++) {
       if (self->pins[i] != NULL) {
         pi_gpio_release(self->pins[i]);
         self->pins[i] = NULL;
