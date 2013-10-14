09315cac951130dd1c7c0c777eafc27093452bfc
Jake Luer <jake@alogicalparadox.com>
2013-10-14 01:00:34 +0000

    examples: [gpio-led] timer in ready

--- examples/led.js
+++ examples/led.js
@@ -1,12 +1,14 @@
 var pidaeus = require('..');
 
-var gpio = pidaeus.bind('gpio');
-var led = gpio.createWriteStream(18);
+var gpio = pidaeus.bind('gpio', function() {
+  var led = gpio.createWriteStream(18);
 
-led.on('release', gpio.teardown.bind(gpio));
+  led.on('release', gpio.teardown.bind(gpio));
 
-led.write(1, function() {
+  led.write(1);
+
   setTimeout(function() {
     led.end(0);
   }, 3000);
 });
+
